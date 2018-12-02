1903659ff8c363dafe97aa9a5e88b3933eedde7c
Quentin Muret <quentin.muret@gmail.com>
2018-12-02 17:12:55 -0500

    possibilité de choisir une université lors de la création d'un compte

--- app/src/main/java/uqam/projetconceptionlogiciel/Activities/CreateAccountActivity.java
+++ app/src/main/java/uqam/projetconceptionlogiciel/Activities/CreateAccountActivity.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import uqam.projetconceptionlogiciel.DAL.IUniversityDAL;
+import uqam.projetconceptionlogiciel.Model.University;
 import uqam.projetconceptionlogiciel.R;
 
 import io.reactivex.functions.Consumer;
@@ -19,18 +29,23 @@ import uqam.projetconceptionlogiciel.APIError.IUserAPIError;
 import uqam.projetconceptionlogiciel.DAL.IUserDAL;
 import uqam.projetconceptionlogiciel.Model.User;
 import uqam.projetconceptionlogiciel.Retrofit.APIError.UserAPIError;
+import uqam.projetconceptionlogiciel.Retrofit.DAL.UniversityDAL;
 import uqam.projetconceptionlogiciel.Retrofit.DAL.UserDAL;
 
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     private IUserDAL userDAL = new UserDAL();
+    IUniversityDAL universityDAL = new UniversityDAL();
+    List ListUniversities = new ArrayList();
+    ArrayAdapter adapter;
 
     // UI references.
     private EditText mEmailView;
     private EditText mPasswordView;
     private EditText firstName;
     private EditText lastName;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +56,44 @@ public class CreateAccountActivity extends AppCompatActivity {
         mPasswordView = (EditText) findViewById(R.id.password);
         firstName = (EditText) findViewById(R.id.first_name);
         lastName = (EditText) findViewById(R.id.last_name);
+        spinner = (Spinner) findViewById(R.id.spinner);
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+                validate();
             }
         });
+
+        this.adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                this.ListUniversities
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        this.GetAllUniversities();
     }
 
-    private void login() {
+    private void GetAllUniversities() {
+        universityDAL.getAllUniversities()
+                .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Response<List<University>>>() {
+            @Override
+            public void accept(Response<List<University>> response) {
+                Integer i;
+                for (i = 0; i< response.body().size(); i++) {
+                    CreateAccountActivity.this.ListUniversities.add(response.body().get(i).getName());
+                    CreateAccountActivity.this.adapter = new ArrayAdapter(
+                            CreateAccountActivity.this,
+                            android.R.layout.simple_spinner_item,
+                            CreateAccountActivity.this.ListUniversities
+                    );
+                }
+            }
+                });
+    }
+
+    private void validate() {
         try {
             PostUserMethod();
         } catch (InterruptedException e) {
